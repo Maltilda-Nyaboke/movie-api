b9124c434c4c37465cef6e362c8a93ca1e20bdac
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 03:34:09 +0300

    style:add new files

--- config.py
+++ config.py
@@ -6,6 +6,28 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:password@localhost/watchlist_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:password@localhost/watchlist'
+    DEBUG = True
+
+
+
+
+
 
 class Config:
     '''
@@ -21,9 +43,8 @@ class Config:
     MOVIE_API_KEY = '<Movie API Key>'
     SECRET_KEY = '<Flask WTF Secret Key>'
     MOVIE_API_KEY = '037c55d6b0ab6774fd8dcb27d1eaca9a'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:New password@localhost/watchlist'
-    
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
+       
 
 class ProdConfig(Config):
     '''
@@ -47,7 +68,8 @@ DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
 
 
--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-export MOVIE_API_KEY = "037c55d6b0ab6774fd8dcb27d1eaca9a"
-export SECRET_KEY = "pyronlks"
+export MOVIE_API_KEY='037c55d6b0ab6774fd8dcb27d1eaca9a'
+export SECRET_KEY='pyronlks'
+export MAIL_USERNAME='maltildabosibori@gmail.com'
+export MAIL_PASSWORD='kingedqueen2020'
+
 
 python3.9 manage.py server
